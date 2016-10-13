8b994f583b0088f0a3edcbd63bf712b91d7afb49
Evgeniy <egladysh@Evgeniys-MacBook-Pro.local>
2016-10-12 22:50:14 -0700

    problem11

---
+++ problem11/main.cpp
@@ -0,0 +1,14 @@
+#include <assert.h>
+#include <iostream>
+#include <limits>
+
+int main()
+{
+	std::cout << std::endl 
+		<< "Project Euler - Problem 1" << std::endl
+		<< "-------------------------" << std::endl;
+
+	std::cout << "Answer: " << r << std::endl << std::endl;
+
+	return 0;
+}
